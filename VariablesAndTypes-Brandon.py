b58ca3118d22317e05101df47acbec9f6af96923
b-andres-hawaiiedu <ba40@hawaii.edu>
2025-02-27 17:16:49 -1000

    Add files via upload

---
+++ VariablesAndTypes-Brandon.py
@@ -0,0 +1,31 @@
+tenthousand = 10000
+three = 3
+zero = 0
+
+print(tenthousand + three)
+print(tenthousand / three)
+
+# print(three / zero) 
+
+# Uncommenting the above comment will cause a traceback as any division by zero has no defined value.
+
+
+hello = 'hello'
+world = 'world'
+
+print(hello + ' ' + world + '!')
+
+# print(hello - world)
+
+# Uncommenting the above comment will cause a traceback as subtracting strings doesn't make much sense here.
+
+
+print(type(three))
+three = float(three)
+
+print(type(three))
+
+# hello = float(hello)
+# print(type(hello))
+
+# Uncommenting the above comment will cause a traceback as the variable is not holding a number.
